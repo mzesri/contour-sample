86cff7c7a37fef9cda1d18cd1aa97a438f46e683
Javier Delgadillo <jdelgadillo@esri.com>
2019-04-10 13:39:34 -0700

    0.2 -> 0.2.1
    Add check for negative delay params to delay endpoint
    Move the echo logic to a scala.template file instead of a string in the controller

--- play/app/controllers/GetController.scala
+++ play/app/controllers/GetController.scala
@@ -1,54 +1,32 @@
 package controllers
 
 import javax.inject.Inject
-import play.api.http.ContentTypes
-import play.api.mvc.{BaseController, ControllerComponents, Request, Result}
-import play.twirl.api.utils.StringEscapeUtils
+import play.api.mvc.{BaseController, ControllerComponents}
 
 class GetController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
   private val LOGGER = play.api.Logger(getClass)
 
-  private def echoRequest(request: Request[_]): String = {
-    val buffer = new StringBuilder()
-    buffer.append("  <tr><th>Header Key</th><th>Header Value</th></tr>\n")
-    request.headers.toSimpleMap.foreach( entry =>
-        buffer.append(s"   <tr><td>${StringEscapeUtils.escapeXml11(entry._1)}</td><td>${StringEscapeUtils.escapeXml11(entry._2)}</td></tr>\n")
-    )
-    buffer.toString()
-  }
-
-  private def echo(request: Request[_]): Result = {
-    Ok(
-      s"""
-         |<!DOCTYPE html>
-         |<html lang="en">
-         | <head><title>Hello World!</title></head>
-         | <body>
-         | <h1>Request:</h1>
-         | <table border="1">
-         | ${echoRequest(request)}
-         | </table>
-         | </body>
-         |</html>
-      """.stripMargin).as(ContentTypes.HTML)
-  }
-
   def index = Action{ request =>
-    echo(request)
+    Ok(views.html.echo(request))
   }
 
   def delay = Action { request =>
     val defDelay = 30
     val delay: Int = request.queryString.get("delay").flatMap(_.headOption) match {
       case Some(delayParam) =>
-        Int(delayParam).getOrElse(defDelay)
+        val delayParamAsInt = Int(delayParam).getOrElse(defDelay)
+        if (delayParamAsInt >= 0) {
+          delayParamAsInt
+        } else {
+          defDelay
+        }
       case _ =>
         defDelay
 
     }
     LOGGER.info(s"Sleeping for $delay seconds before echoing request....")
     Thread.sleep(delay*1000)
-    echo(request)
+    Ok(views.html.echo(request))
   }
 
 }
